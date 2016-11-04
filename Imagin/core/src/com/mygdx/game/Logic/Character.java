5729aa65b77a965b9ddbd1eaa530fe6dbf244bd4
JoaoChaves96 <joaovitor1906@hotmail.com>
2016-11-04 20:34:46 +0000

    moves implemented on all characters

--- Imagin/core/src/com/mygdx/game/Logic/Character.java
+++ Imagin/core/src/com/mygdx/game/Logic/Character.java
@@ -8,6 +8,7 @@ import com.badlogic.gdx.math.Rectangle;
 import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.physics.box2d.Body;
 import com.badlogic.gdx.physics.box2d.BodyDef;
+import com.badlogic.gdx.physics.box2d.Box2DDebugRenderer;
 import com.badlogic.gdx.physics.box2d.FixtureDef;
 import com.badlogic.gdx.physics.box2d.PolygonShape;
 import com.badlogic.gdx.physics.box2d.World;
@@ -19,22 +20,24 @@ public abstract class Character extends Sprite{
     protected Vector2 position;
    // protected Animation animation;
     protected Texture texture;
-    protected World world;
+    public World world;
     protected Body body;
     protected BodyDef bdef;
     public Rectangle rectangle;
+    public Box2DDebugRenderer b2d;
 
 
     protected Character(float x, float y, int frames, World world){
         position = new Vector2(x, y);
         this.world = world;
         defineCharacter();
-        texture = new Texture("badlogic.jpg");
+        texture = new Texture("bush.png");
 
         //animation = new Animation(new TextureRegion(texture), frames, 0.5f);
     }
 
     public void defineCharacter(){
+        b2d = new Box2DDebugRenderer();
         bdef = new BodyDef();
         bdef.position.set(position.x,position.y);
         bdef.type = BodyDef.BodyType.DynamicBody;
@@ -48,7 +51,7 @@ public abstract class Character extends Sprite{
         body.createFixture(fdef).setUserData(this);
 
         rectangle = new Rectangle(0,0,32,32);
-        rectangle.set(body.getPosition().x -16, body.getPosition().y -16,32,32);
+       // rectangle.set(body.getPosition().x -16, body.getPosition().y -16,32,32);
     }
 
     public abstract float getPositionX();
@@ -58,4 +61,8 @@ public abstract class Character extends Sprite{
     public abstract void update(float dt);
     public abstract void dispose();
     public abstract Rectangle getBoundaries();
+    public abstract void moveUp();
+    public abstract void moveDown();
+    public abstract void moveRight();
+    public abstract void moveLeft();
 }
