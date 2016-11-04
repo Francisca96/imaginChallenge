27b6fb5576f6c62244022044a20312fec41740c6
Tomás Vieira Caldas <tomas_v_caldas@hotmail.com>
2016-11-04 17:15:53 +0000

    Character shape created

--- Imagin/android/src/com/mygdx/game/Logic/Character.java
+++ Imagin/android/src/com/mygdx/game/Logic/Character.java
@@ -2,19 +2,44 @@ package com.mygdx.game.Logic;
 
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.Animation;
+import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import com.badlogic.gdx.math.Rectangle;
 import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.physics.box2d.Body;
+import com.badlogic.gdx.physics.box2d.BodyDef;
+import com.badlogic.gdx.physics.box2d.FixtureDef;
+import com.badlogic.gdx.physics.box2d.PolygonShape;
+import com.badlogic.gdx.physics.box2d.World;
 
 /**
  * Created by Joao on 04-11-2016.
  */
-public abstract class Character {
+public abstract class Character extends Sprite{
     protected Vector2 position;
     protected Animation animation;
     protected Texture texture;
+    protected World world;
+    protected Body body;
 
-    protected Character(float x, float y, int frames){
+
+    protected Character(float x, float y, int frames, World world){
         position = new Vector2(x, y);
+        this.world = world;
+        BodyDef bdef = new BodyDef();
+        bdef.position.set(position.x,position.y);
+        bdef.type = BodyDef.BodyType.DynamicBody;
+        body = world.createBody(bdef);
+
+        FixtureDef fdef = new FixtureDef();
+        PolygonShape shape = new PolygonShape();
+        fdef.shape = shape;
+        shape.setAsBox(16,16);
+
+        body.createFixture(fdef).setUserData(this);
+
+
+
 
         texture = new Texture("img");
 
