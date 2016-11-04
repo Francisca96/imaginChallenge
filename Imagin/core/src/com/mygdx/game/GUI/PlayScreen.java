34efabba330b07c4f831a9c3116b4e3addb3657a
Ines Ferreira <inesferreira789@gmail.com>
2016-11-04 22:09:10 +0000

    Changing to PlayScreen when clicking on button Play

--- Imagin/core/src/com/mygdx/game/GUI/PlayScreen.java
+++ Imagin/core/src/com/mygdx/game/GUI/PlayScreen.java
@@ -1,5 +1,9 @@
 package com.mygdx.game.GUI;
 
+/**
+ * Created by Inês on 04/11/2016.
+ */
+
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.GL20;
@@ -15,41 +19,22 @@ import com.mygdx.game.Imagin;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
 
-/**
- * Created by Inês on 04/11/2016.
- */
-public class MenuScreen implements Screen{
+public class PlayScreen implements Screen {
 
     private Imagin game;
     private OrthographicCamera cam;
     private Viewport menuPort;
     private Stage stage;
 
-    //Variables of textures
     private Texture background;
-    private Texture title;
-    private Texture guy;
     private TextureAtlas lvlMenuAtlas;
     private Skin skin;
 
-    public static int title_width = Imagin.V_WIDTH - 100;
-    public static int title_height = 100;
-    public static int guy_width = 270;
-    public static int guy_height = 270;
-    public static int btn_width = 400;
-    public static int btn_height = 200;
 
 
-    //Inicializar botoes
-    private ImageButton playBtn;
-    private ImageButton storeBtn;
-    private ImageButton soundBtn;
-
-    public MenuScreen(Imagin game){
+    public PlayScreen(Imagin game) {
         this.game = game;
         this.background = new Texture("background.jpg");
-        this.title = new Texture("title1.png");
-        this.guy = new Texture("guymenu.png");
 
         cam = new OrthographicCamera();
         cam.setToOrtho(false);
@@ -59,9 +44,6 @@ public class MenuScreen implements Screen{
     }
 
     public void handleInput(float dt) {
-
-        if (playBtn.isPressed()) game.setScreen(new PlayScreen(game));
-
     }
 
     public void update(float dt){
@@ -75,29 +57,29 @@ public class MenuScreen implements Screen{
 
     @Override
     public void render(float delta) {
+
         update(delta);
         Gdx.gl.glClearColor(0, 0, 0, 1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
         game.batch.begin();
         game.batch.draw(background,0, 0, Imagin.V_WIDTH, Imagin.V_HEIGHT);
-        game.batch.draw(title,Imagin.V_WIDTH / 2 - title_width /2, Imagin.V_HEIGHT / 2 + 200, title_width, title_height);
-        game.batch.draw(guy, Imagin.V_WIDTH/2 - guy_width/2+90, Imagin.V_HEIGHT/2-400, guy_width, guy_height);
         game.batch.end();
         stage.draw();
+
     }
 
     @Override
     public void resize(int width, int height) {
-        menuPort.update(width, height);
-    }
-
-    @Override
-    public void pause() {
 
     }
 
     @Override
     public void resume() {
+
+    }
+
+    @Override
+    public void pause() {
 
     }
 
@@ -118,28 +100,7 @@ public class MenuScreen implements Screen{
     public void initStage(SpriteBatch batch) {
         this.stage = new Stage(menuPort, batch);
 
-        lvlMenuAtlas = new TextureAtlas("buttons_menu.pack");
-        skin = new Skin();
-        skin.addRegions(lvlMenuAtlas);
-        stage.clear();
-
-        //PlayButton
-        playBtn = new ImageButton(skin.getDrawable("playBtn"));
-        playBtn.setSize(btn_width,btn_height);
-        playBtn.setPosition(Imagin.V_WIDTH /2 - playBtn.getWidth()/2,Imagin.V_HEIGHT /2 - 20);
-        stage.addActor(playBtn);
-
-        //OptionsButton
-        storeBtn = new ImageButton(skin.getDrawable("storeBtn"));
-        storeBtn.setSize(btn_width,btn_height);
-        storeBtn.setPosition(Imagin.V_WIDTH /2 - storeBtn.getWidth()/2, Imagin.V_HEIGHT/2 - 120);
-        stage.addActor(storeBtn);
-
-        soundBtn = new ImageButton(skin.getDrawable("soundon"));
-        soundBtn.setSize(80,80);
-        soundBtn.setPosition(Imagin.V_WIDTH /2 - soundBtn.getWidth()/2-130, Imagin.V_HEIGHT/2 - 310);
-        stage.addActor(soundBtn);
-
         Gdx.input.setInputProcessor(stage);
     }
+
 }
--- Imagin/core/src/com/mygdx/game/Imagin.java
+++ Imagin/core/src/com/mygdx/game/Imagin.java
@@ -5,6 +5,7 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.mygdx.game.GUI.MenuScreen;
+import com.mygdx.game.GUI.PlayScreen;
 
 public class Imagin extends Game {
 	public SpriteBatch batch;
