102a3c2c56ebd7e982edcd7ac78324220790aca9
Ines Ferreira <inesferreira789@gmail.com>
2016-11-05 04:05:37 +0000

    Redesign of MenuScreen

--- Imagin/core/src/com/mygdx/game/Imagin.java
+++ Imagin/core/src/com/mygdx/game/Imagin.java
@@ -1,24 +1,21 @@
 package com.mygdx.game;
 
 import com.badlogic.gdx.Game;
-import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.mygdx.game.GUI.MenuScreen;
-import com.mygdx.game.GUI.PlayScreen;
-import com.mygdx.game.GUI.StoreScreen;
 
 public class Imagin extends Game {
 	public SpriteBatch batch;
 	public static final int V_WIDTH = 480;
 	public static final int V_HEIGHT = 800;
 	public static final float PPM = 100;
+
 	
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
 
-		setScreen(new StoreScreen(this));
+		setScreen(new MenuScreen(this));
 	}
 
 	@Override
